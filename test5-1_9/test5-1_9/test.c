223a0c377a7bb3a34d47a744fef94d382eda20d7
roaring-black-fertilizer <1766155164@qq.com>
2025-01-12 19:08:27 +0800

    指针进阶3

---
+++ test5-1_9/test5-1_9/test.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	int a[] = { 1,2,3,4 };
+//	printf("%d\n", sizeof(a));//16
+//	printf("%d\n", sizeof(a + 0));//
+//	printf("%d\n", sizeof(*a));//1
+//	printf("%d\n", sizeof(a + 1));
+//	printf("%d\n", sizeof(a[1]));
+//	printf("%d\n", sizeof(&a));
+//	printf("%d\n", sizeof(*&a));
+//	printf("%d\n", sizeof(&a + 1));
+//	printf("%d\n", sizeof(&a[0]));
+//	printf("%d\n", sizeof(&a[0] + 1));
+//	return 0;
+//}
+//int main()
+//{
+//	char arr[] = { 'a','b','c','d','e','f' };
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(arr + 0));
+//	printf("%d\n", sizeof(*arr));
+//	printf("%d\n", sizeof(arr[1]));
+//	printf("%d\n", sizeof(&arr));
+//	printf("%d\n", sizeof(&arr + 1));
+//	printf("%d\n", sizeof(&arr[0] + 1));
+//	printf("%d\n", strlen(arr));
+//	printf("%d\n", strlen(arr + 0));
+//	printf("%d\n", strlen(*arr));
+//	printf("%d\n", strlen(arr[1]));
+//	printf("%d\n", strlen(&arr));
+//	printf("%d\n", strlen(&arr + 1));
+//	printf("%d\n", strlen(&arr[0] + 1));
+//	return 0;
+//}
+int main()
+{
+	int a[5] = { 1, 2, 3, 4, 5 };
+	int* ptr = (int*)(&a + 1); 
+	printf("%d,%d", *(a + 1), *(ptr - 1));
+	return 0;
+}
